0535bf111c3b8952f0350d779446adabdd968e9c
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-11-10 02:30:22 -0800

    Socket server pushing notice to mobile app

---
+++ mahrio.js
@@ -0,0 +1,11 @@
+process.env.NODE_URL = '192.168.0.6';
+process.env.NODE_PUBLIC_PATH = './';
+
+require('mahrio').runServer(process.env, __dirname).then(function (server) {
+  var io = require('socket.io').listen(server.listener);
+
+  var state = 1;
+  setInterval(function () {
+    io.sockets.emit('blink:led', state = !state);
+  }, 3000);
+});
